0c5a0e2168c453edc132761d35b9ad6b22dea153
Ivan Sharavuev <i.sharavuev@omprussia.ru>
2021-03-18 23:11:39 +0300

    Format files.

--- task1/uniq_rand_numbers.cpp
+++ task1/uniq_rand_numbers.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <ctime>
+#include <iostream>
 
 using namespace std;
 
@@ -7,37 +7,35 @@ using namespace std;
     Fill an array of length 10 by uniq random values
 */
 
-int main()
-{
-    srand(time(NULL));
+int main() {
+  srand(time(NULL));
 
-    const size_t ARRAY_LENGTH = 10;
-    const int VALUE_THRESH = 20;
+  const size_t ARRAY_LENGTH = 10;
+  const int VALUE_THRESH = 20;
 
-    int arr[ARRAY_LENGTH];
+  int arr[ARRAY_LENGTH];
 
-    for (int i = 0; i < ARRAY_LENGTH; i++){
-        bool goodNumber;
-        do{
-            goodNumber = true;
-            int newNumber = rand() % VALUE_THRESH;
-            for (int j = 0; j < i; j++){
-                if (arr[j] == newNumber){
-                    goodNumber = false;
-                    break;
-                }
-            }
-            if (goodNumber){
-                arr [i] = newNumber;
-            }
-        }while(!goodNumber);
-        
-    }
+  for (int i = 0; i < ARRAY_LENGTH; i++) {
+    bool goodNumber;
+    do {
+      goodNumber = true;
+      int newNumber = rand() % VALUE_THRESH;
+      for (int j = 0; j < i; j++) {
+        if (arr[j] == newNumber) {
+          goodNumber = false;
+          break;
+        }
+      }
+      if (goodNumber) {
+        arr[i] = newNumber;
+      }
+    } while (!goodNumber);
+  }
 
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+  for (int i = 0; i < ARRAY_LENGTH; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 
-    return 0;
+  return 0;
 }
--- task2/swap.cpp
+++ task2/swap.cpp
@@ -8,35 +8,31 @@ using namespace std;
       2) References
 */
 
-void p_swap(int *a,int *b)
-{
-    int c = *a;
-    *a = *b;
-    *b = c;
+void p_swap(int *a, int *b) {
+  int c = *a;
+  *a = *b;
+  *b = c;
 }
 
-template <class T>
-void r_swap(T &a, T &b)
-{
-    T c = a;
-    a = b;
-    b = c;
+template <class T> void r_swap(T &a, T &b) {
+  T c = a;
+  a = b;
+  b = c;
 }
 
-int main()
-{
-    int a = 5;
-    int b = 6;
+int main() {
+  int a = 5;
+  int b = 6;
 
-    cout << "a = "<<  a  << " b = "<< b << endl << endl;
+  cout << "a = " << a << " b = " << b << endl << endl;
 
-    cout << "swap a & b using pointers" << endl;
-    p_swap(&a,&b);
-    
-    cout << "a = "<<  a  << " b = "<< b << endl << endl;
+  cout << "swap a & b using pointers" << endl;
+  p_swap(&a, &b);
 
-    cout << "swap a & b using references" << endl;
-    r_swap(a,b);
-    
-    cout << "a = "<<  a  << " b = "<< b << endl << endl;
+  cout << "a = " << a << " b = " << b << endl << endl;
+
+  cout << "swap a & b using references" << endl;
+  r_swap(a, b);
+
+  cout << "a = " << a << " b = " << b << endl << endl;
 }
